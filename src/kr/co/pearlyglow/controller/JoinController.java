e3503f103004506adef30f0d4718154c320105ab
admin <admin@DESKTOP-60HT7MK>
2020-12-22 11:03:32 +0900

    회원가입 페이지 이동 수정

--- src/kr/co/pearlyglow/controller/JoinController.java
+++ src/kr/co/pearlyglow/controller/JoinController.java
@@ -39,6 +39,6 @@ public class JoinController extends HttpServlet{
 		}
 		
 		req.setAttribute("code", resultCode);
-		req.getRequestDispatcher("/index.jsp?spage=Member/result.jsp").forward(req, resp);
+		req.getRequestDispatcher("/index.jsp?spage=Member/joinResult.jsp").forward(req, resp);
 	}
 }
