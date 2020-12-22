0065cf9d6919b2e7751ba0e388184575af7b07f7
admin <admin@DESKTOP-60HT7MK>
2020-12-22 10:19:48 +0900

    오타수정

--- src/kr/co/peralyglow/DAO/Gmail.java
+++ src/kr/co/peralyglow/DAO/Gmail.java
@@ -6,7 +6,6 @@ import javax.mail.PasswordAuthentication;
 public class Gmail extends Authenticator{
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("cssimm@gmail.com", "cs9059666"); //Íµ¨Í?Í≥ÑÏ†ï
-		//return new PasswordAuthentication("?ïÑ?ù¥?îî", "ÎπÑÎ?Î≤àÌò∏"); //Íµ¨Í?Í≥ÑÏ†ï
+		return new PasswordAuthentication("±∏±€¿Ã∏ﬁ¿œ", "±∏±€∫Òπ–π¯»£");
 	}
 }
